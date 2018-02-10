fd195ca53efabf9f9d3b224da7f12cab3a845c6b
agusbandung <agusana1544@gmail.com>
2018-02-10 18:29:14 +0700

    pms

--- routes/index.js
+++ routes/index.js
@@ -1,153 +1,82 @@
 var express = require('express');
 var router = express.Router();
-let pg = require('pg');
-let conString = "postgres://postgres:1234@localhost:5432/pms";
-let client = new pg.Client(conString);
-const bodyParser = require('body-parser');
+var passwordHash = require('password-hash');
 
+module.exports = function(db) {
+  router.get('/', function(req, res, next) {
+    var message = new Array(req.flash('loginMessage')[0])
+    res.render('login', { title: 'Login Page', message: message } );
+  });
 
-/* GET home page. */
-
-router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Login Page' } );
-});
-
-// router.get('/', function(req, res, next) {
-//   client.connect();
-//   let url = (req.url == "/") ? "/?page=1" :req.url;
-//   let page = Number(req.query.page) || 1;
-//   if(url.indexOf('&search=') != -1){
-//     page = 1;
-//   }
-//   url = url.replace('&search=', '')
-//   let params = []
-//   let isfilter = false;
-//   let sql = 'SELECT count(*) as total FROM vproject'
-//   if(req.query.cid && req.query.id){
-//     params.push(`projectid = '${req.query.projectid}'`);
-//     isfilter = true;
-//   }
-//   if (req.query.cname && req.query.name) {
-//     params.push(`name = '${req.query.name}'`);
-//     isfilter = true;
-//   }
-//   if (req.query.cmember && req.query.member) {
-//     params.push(` = '${req.query.member}'`);
-//     isfilter = true;
-//   }
-//
-//   if (isfilter) {
-//
-//     sql += ' WHERE ' + params.join(' AND ')
-//   }
-//
-//   //menghitung semua data yang ada di Database
-//   client.query(sql, (err, data)=>{
-//     if(err){
-//       return data.send(err);
-//     }
-//
-//     //pagination
-//
-//     let limit = 3
-//     let offset = (page-1) * 3
-//     let total = data.rows[0].total;
-//     let pages = (total == 0) ? 1 : Math.ceil(total/limit);
-//     sql = "SELECT * FROM vproject";
-//     if(isfilter){
-//       sql += ' WHERE ' + params.join(' AND ')
-//     }
-//     sql += ` LIMIT ${limit} OFFSET ${offset}`;
-//     client.query(sql, (err, data) => {
-//       res.render('project',{title:"pms",rows : data.rows, query : req.query, pagination:{page: page, limit: limit, offset: offset, pages: pages, total: total, url: url}});
-//
-//     })
-//   });
-// });
-
-router.post('/', function(req, res){
-  //mengambil data user dari Database
-  let email = req.body.email;
-  let pass = req.body.password;
-  client.connect();
-  client.query("SELECT * FROM users WHERE email = $1 and password = $2", [email, pass],(err, data)=>{
-   if(err) {
-     return res.send(err);
-   }
-
-//  if(emid == data.rows['email'] && pass == data.rows['password']){
-     if(data.rows.length > 0){
-     req.session.email = email;
-     res.redirect('/project')
-   }else{
-     res.redirect('/')
-   }
- });
-})
-
-router.get('/project', function(req, res){
-  if(req.session.email){
-    res.render('project', { title: 'Express' });
-  }else{
-    res.redirect('/')
-  }
-
-})
-
-router.post('/index', function(req, res){
-  //mengambil data user dari Database
-  let email = req.body.email;
-  let pass = req.body.password;
-  client.query("SELECT * FROM users WHERE email = $1 and password = $2", [email, pass],(err, data)=>{
-   if(err) {
-     return res.send(err);
-   }
-   if(data.rows.length > 0){
-     req.session.email = email;
-     res.redirect('/project')
-   }else{
-     res.redirect('/')
-   }
- });
-})
-
-router.get('/add', function(req, res, next){
-  client.query(`select * from users order by firstname`, (err, data)=>{
-    res.render('add',{data:data.rows});
-})
-
-router.post('/add', function(req, res, next){
-  let isi = req.body;
-  let tambah =  "insert into projects(name) values($1)";
-   client.query(tambah, [isi.name], (err, data) => {
-      if (err) {
-        return res.send(err);
+  router.post('/', function(req, res, next) {
+    db.query(`SELECT * FROM users WHERE email = '${req.body.email}'`, (err, data) => {
+      if(err) {
+        console.error(err);
+        req.flash('loginMessage', 'email anda salah..!');
+        return res.redirect('/')
+      }
+      if(data.rows.length > 0) {
+        //login masuk
+        if(req.body.pass == data.rows[0].password) {
+          //console.log('haiiii');
+            delete data.rows[0].password;
+            req.session.user = data.rows[0]
+            return res.redirect('/projects')
+        } else {
+            req.flash('loginMessage', 'password tidak benar..!');
+            return res.redirect('/')
         }
-        res.redirect('/projects');
-  })
-})
-})
-router.get ('/delete/:id', function(req,res) {
-  var id = req.params.id
-  client.query (`DELETE FROM vproject Where projectid= '${id}'`, (err,rows) =>{
-    if(err) {
-      console.error(err)
-      return res.send(err);
+
+      } else {
+        req.flash('loginMessage', "email tidak terdaftar..!")
+        return res.redirect('/')
+      }
+    });
+  });
+
+  router.get('/register', function(req, res, next) {
+    var message = new Array(req.flash('registerMessage')[0])
+    res.render('register', { title: 'Register Account', message: message } );
+  });
+
+  router.post('/register', function(req, res, next) {
+    if(req.body.pass !== req.body.repass) {
+      req.flash('registerMessage', 'password tidak sesuai');
+      return res.redirect('/register')
     }
-    if(rows.length > 0){
-      res.render('delete', {item: rows[0]});
-    }else{
+
+
+    db.query(`SELECT email FROM users WHERE email = '${req.body.email}'`, (err, data) => {
+      if(err) {
+        console.error(err);
+        req.flash('registerMessage', 'terjadi kesalahan, hubungi administrator');
+        return res.redirect('/register')
+      }
+      console.log(data);
+      if(data.rows.length > 0) {
+        req.flash('registerMessage', 'email udah terdaftar');
+        return res.redirect('/register')
+      } else {
+        //passwordHash.generate
+        db.query(`INSERT INTO users(email, password, projectcolumns, membercolumns, issuescolumns, role) VALUES('${req.body.email}', '${(req.body.pass)}', '{}', '{}', '{}', 'User')`, (err, data) => {
+          if(err) {
+            console.error(err);
+            req.flash('registerMessage', 'terjadi kesalahan, hubungi administrator');
+            return res.redirect('/register')
+          }
+          req.flash('registerMessage', 'registrasi berhasil, silahkan login');
+          return res.redirect('/register')
+        });
+      }
+    });
+  });
+
+  router.get('/logout', function(req, res, next) {
+    req.session.destroy(function() {
       res.redirect('/');
-    }
-  })
-})
+    });
+  });
 
-
-router.get('/logout', function(req, res){
-  req.session.destroy(function(){
-    res.redirect('/')
-  })
-})
-
-
-module.exports = router;
+  return router;
+}
+/* GET home page. */
